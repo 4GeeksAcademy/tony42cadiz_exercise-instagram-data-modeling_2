14836587f510f6aee6032eb29fa68158c6d7a003
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2025-02-11 07:23:31 +0000

    Exercise-Instagram-Data-Modeling_2

--- src/models.py
+++ src/models.py
@@ -1,32 +1,71 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    accepted = Column(Boolean)
 
-    def to_dict(self):
-        return {}
+    # Relación: Muchos seguidores pertenecen a un usuario
+    follower_id = Column(Integer, ForeignKey('users.id'))
+    follower = relationship('Users', foreign_keys=[follower_id])
+    # Relación: Muchos seguidores están asociados a un usuario
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', foreign_keys=[user_id])
+    
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(50))
+    description = Column(String(250))
+
+    # Relación: Muchos posts pertenecen a un usuario
+    user_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
+    
+    # Relación: Un post puede tener muchos comentarios
+    post_id_comments = Column(Integer, ForeignKey('posts.id'))
+    comments = relationship('Comments')  
+     
+    
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(255), nullable=False)
+    type = Column(String(50), nullable=False)
+
+    # Relación: Muchos medios pertenecen a un usuario
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)  
+    
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250))
+
+    # Relación: Muchos comentarios pertenecen a un usuario
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)  
+
+    # Relación: Muchos comentarios están asociados a un post
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship(Post)   
 
 ## Draw from SQLAlchemy base
 try:
@@ -35,3 +74,4 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
